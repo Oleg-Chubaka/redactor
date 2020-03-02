ffebf5fa67242de0d02c17890eb757e09380c034
Oleh WebDev <oleh.webdev@gmail.com>
2020-03-02 20:25:56 +0200

    0.1.9

--- js/canvas.js
+++ js/canvas.js
@@ -31,20 +31,24 @@
     var ch = canvas.height;
     
     var lh1 = m(hp(17), 80);
+    var lh2 = 25;
+    var lh3 = lh2 - 10;
 
     var lw1 = m(wp(20), 200);
     var lw2 = m(wp(4), 40);
-    var lw3 = cw - (lw1 + 160 + lw2 + 30)*2;
-    
+    var lw4 = 40;
+    var lw5 = lw4 - 20;
+    var lw3 = cw - (lw1 + lw4 + 60 - lh3 + 15 + lh2 + 5 + lw2 + 30)*2;
+
     // BLACK BG
 
     ctx.beginPath();
-    ctx.moveTo(50, 40);
-    ctx.lineTo(50, lh1 + 40);
-    ctx.lineTo(30, lh1 + 70);
-    ctx.lineTo(30, ch - lh1 - 70);
-    ctx.lineTo(50, ch - lh1 - 40);
-    ctx.lineTo(50, ch - 40);
+    ctx.moveTo(lw4, lh2);
+    ctx.lineTo(lw4, lh1 + lh2);
+    ctx.lineTo(lw5, lh1 + lh2 + 30);
+    ctx.lineTo(lw5, ch - lh1 - lh2 - 30);
+    ctx.lineTo(lw4, ch - lh1 - lh2);
+    ctx.lineTo(lw4, ch - lh2);
     ctx.lineTo(0, ch);
     ctx.lineTo(0, 0);
     ctx.closePath();
@@ -52,59 +56,58 @@
 
     ctx.beginPath();
     ctx.moveTo(0, ch);
-    ctx.lineTo(50, ch - 40);
-    ctx.lineTo(lw1 + 50, ch - 40);
-    ctx.lineTo(lw1 + 80, ch - 20);
-    ctx.lineTo(lw1 + 130, ch - 20);
-    ctx.lineTo(lw1 + 110, ch);
+    ctx.lineTo(lw4, ch - lh2);
+    ctx.lineTo(lw1 + lw4, ch - lh2);
+    ctx.lineTo(lw1 + lw4 + 15, ch - lh3);
+    ctx.lineTo(lw1 + lw4 + 60, ch - lh3);
+    ctx.lineTo(lw1 + lw4 + 60 - lh3, ch);
     ctx.closePath();
     ctx.fill();
     
     ctx.beginPath();
-    ctx.moveTo(lw1 + 125, ch);
-    ctx.lineTo(lw1 + 160, ch - 35);
-    ctx.lineTo(lw1 + 160 + lw2, ch - 35);
+    ctx.moveTo(lw1 + lw4 + 60 - lh3 + 15, ch);
+    ctx.lineTo(lw1 + lw4 + 60 - lh3 + 15 + lh2 + 5, ch - lh2 - 5);
+    ctx.lineTo(lw1 + lw4 + 60 - lh3 + 15 + lh2 + 5 + lw2, ch - lh2 - 5);
     if (lw3 > 100) {
-      ctx.lineTo(lw1 + 160 + lw2 + 30, ch - 15);
-      ctx.lineTo(lw1 + 160 + lw2 + 30 + lw3, ch - 15);
+      ctx.lineTo(lw1 + lw4 + 60 - lh3 + 15 + lh2 + 5 + lw2 + 30, ch - lh3);
+      ctx.lineTo(lw1 + lw4 + 60 - lh3 + 15 + lh2 + 5 + lw2 + 30 + lw3, ch - lh3);
     }
-    ctx.lineTo(lw1 + 160 + lw2 + 30 + lw3 + 30, ch - 35);
-    ctx.lineTo(lw1 + 160 + lw2 + 30 + lw3 + 30 + lw2, ch - 35);
-    ctx.lineTo(lw1 + 160 + lw2 + 30 + lw3 + 30 + lw2 + 35, ch);
+    ctx.lineTo(cw - (lw1 + lw4 + 60 - lh3 + 15 + lh2 + 5 + lw2), ch - lh2 - 5);
+    ctx.lineTo(cw - (lw1 + lw4 + 60 - lh3 + 15 + lh2 + 5), ch - lh2 - 5);
+    ctx.lineTo(cw - (lw1 + lw4 + 60 - lh3 + 15), ch);
     ctx.closePath();
     ctx.fill();
 
     ctx.beginPath();
-    ctx.moveTo(lw1 + 160 + lw2 + 30 + lw3 + 30 + lw2 + 35 + 15, ch);
-    ctx.lineTo(lw1 + 160 + lw2 + 30 + lw3 + 30 + lw2 + 30, ch - 20);
-    ctx.lineTo(lw1 + 160 + lw2 + 30 + lw3 + 30 + lw2 + 30 + 50, ch - 20);
-    ctx.lineTo(lw1 + 160 + lw2 + 30 + lw3 + 30 + lw2 + 30 + 50 + 30, ch - 40);
-    ctx.lineTo(lw1 + 160 + lw2 + 30 + lw3 + 30 + lw2 + 30 + 50 + 30 + lw1, ch - 40);
+    ctx.moveTo(cw - (lw1 + lw4 + 60 - lh3), ch);
+    ctx.lineTo(cw - (lw1 + lw4 + 60), ch - lh3);
+    ctx.lineTo(cw - (lw1 + lw4 + 15), ch - lh3);
+    ctx.lineTo(cw - (lw1 + lw4), ch - lh2);
+    ctx.lineTo(cw - lw4, ch - lh2);
     ctx.lineTo(cw, ch);
     ctx.closePath();
     ctx.fill();
 
     ctx.beginPath();
     ctx.moveTo(cw, ch);
-    ctx.lineTo(cw - 50, ch - 40);
-    ctx.lineTo(cw - 50, ch - lh1 - 40);
-    ctx.lineTo(cw - 30, ch - lh1 - 70);
-    ctx.lineTo(cw - 30, lh1 + 70);
-    ctx.lineTo(cw - 50, lh1 + 40);
-    ctx.lineTo(cw - 50, 40);
+    ctx.lineTo(cw - lw4, ch - lh2);
+    ctx.lineTo(cw - lw4, ch - lh1 - lh2);
+    ctx.lineTo(cw - lw5, ch - lh1 - lh2 - 30);
+    ctx.lineTo(cw - lw5, lh1 + lh2 + 30);
+    ctx.lineTo(cw - lw4, lh1 + lh2);
+    ctx.lineTo(cw - lw4, lh2);
     ctx.lineTo(cw, 0);
     ctx.closePath();
     ctx.fill();
 
     ctx.beginPath();
     ctx.moveTo(cw, 0);
-    ctx.lineTo(cw - 50, 40);
-    ctx.lineTo(cw - lw1 - 70, 40);
-    ctx.lineTo(cw - lw1 - 100, 20);
-    ctx.lineTo(cw - lw1 - 150, 20);
-    ctx.lineTo(lw1 + 100, 20);
-    ctx.lineTo(lw1 + 70, 40);
-    ctx.lineTo(50, 40);
+    ctx.lineTo(cw - lw4, lh2);
+    ctx.lineTo(cw - (lw1 + lw4), lh2);
+    ctx.lineTo(cw - (lw1 + lw4 + 15), lh3);
+    ctx.lineTo(lw1 + lw4 + 15, lh3);
+    ctx.lineTo(lw1 + lw4, lh2);
+    ctx.lineTo(lw4, lh2);
     ctx.lineTo(0, 0);
     ctx.closePath();
     ctx.fill();
@@ -118,49 +121,48 @@
     ctx.shadowBlur = 8;
     ctx.shadowColor = '#197ba8';
 
-    ctx.moveTo(50, 40);
+    ctx.moveTo(lw4, lh2);
     
-    ctx.lineTo(50, lh1 + 40);
-    ctx.lineTo(30, lh1 + 70);
-    ctx.lineTo(30, ch - lh1 - 70);
-    ctx.lineTo(50, ch - lh1 - 40);
-    ctx.lineTo(50, ch - 40);
+    ctx.lineTo(lw4, lh1 + lh2);
+    ctx.lineTo(lw5, lh1 + lh2 + 30);
+    ctx.lineTo(lw5, ch - lh1 - lh2 - 30);
+    ctx.lineTo(lw4, ch - lh1 - lh2);
+    ctx.lineTo(lw4, ch - lh2);
 
-    ctx.lineTo(lw1 + 50, ch - 40);
-    ctx.lineTo(lw1 + 80, ch - 20);
-    ctx.lineTo(lw1 + 130, ch - 20);
-    ctx.lineTo(lw1 + 110, ch);
+    ctx.lineTo(lw1 + lw4, ch - lh2);
+    ctx.lineTo(lw1 + lw4 + 15, ch - lh3);
+    ctx.lineTo(lw1 + lw4 + 60, ch - lh3);
+    ctx.lineTo(lw1 + lw4 + 60 - lh3, ch);
     
-    ctx.moveTo(lw1 + 125, ch);
-    ctx.lineTo(lw1 + 160, ch - 35);
-    ctx.lineTo(lw1 + 160 + lw2, ch - 35);
+    ctx.moveTo(lw1 + lw4 + 60 - lh3 + 15, ch);
+    ctx.lineTo(lw1 + lw4 + 60 - lh3 + 15 + lh2 + 5, ch - lh2 - 5);
+    ctx.lineTo(lw1 + lw4 + 60 - lh3 + 15 + lh2 + 5 + lw2, ch - lh2 - 5);
     if (lw3 > 100) {
-      ctx.lineTo(lw1 + 160 + lw2 + 30, ch - 15);
-      ctx.lineTo(lw1 + 160 + lw2 + 30 + lw3, ch - 15);
+      ctx.lineTo(lw1 + lw4 + 60 - lh3 + 15 + lh2 + 5 + lw2 + 30, ch - lh3);
+      ctx.lineTo(lw1 + lw4 + 60 - lh3 + 15 + lh2 + 5 + lw2 + 30 + lw3, ch - lh3);
     }
-    ctx.lineTo(lw1 + 160 + lw2 + 30 + lw3 + 30, ch - 35);
-    ctx.lineTo(lw1 + 160 + lw2 + 30 + lw3 + 30 + lw2, ch - 35);
-    ctx.lineTo(lw1 + 160 + lw2 + 30 + lw3 + 30 + lw2 + 35, ch);
+    ctx.lineTo(cw - (lw1 + lw4 + 60 - lh3 + 15 + lh2 + 5 + lw2), ch - lh2 - 5);
+    ctx.lineTo(cw - (lw1 + lw4 + 60 - lh3 + 15 + lh2 + 5), ch - lh2 - 5);
+    ctx.lineTo(cw - (lw1 + lw4 + 60 - lh3 + 15), ch);
 
-    ctx.moveTo(lw1 + 160 + lw2 + 30 + lw3 + 30 + lw2 + 35 + 15, ch);
-    ctx.lineTo(lw1 + 160 + lw2 + 30 + lw3 + 30 + lw2 + 30, ch - 20);
-    ctx.lineTo(lw1 + 160 + lw2 + 30 + lw3 + 30 + lw2 + 30 + 50, ch - 20);
-    ctx.lineTo(lw1 + 160 + lw2 + 30 + lw3 + 30 + lw2 + 30 + 50 + 30, ch - 40);
-    ctx.lineTo(lw1 + 160 + lw2 + 30 + lw3 + 30 + lw2 + 30 + 50 + 30 + lw1, ch - 40);
+    ctx.moveTo(cw - (lw1 + lw4 + 60 - lh3), ch);
+    ctx.lineTo(cw - (lw1 + lw4 + 60), ch - lh3);
+    ctx.lineTo(cw - (lw1 + lw4 + 15), ch - lh3);
+    ctx.lineTo(cw - (lw1 + lw4), ch - lh2);
+    ctx.lineTo(cw - lw4, ch - lh2);
 
-    ctx.lineTo(cw - 50, ch - 40);
-    ctx.lineTo(cw - 50, ch - lh1 - 40);
-    ctx.lineTo(cw - 30, ch - lh1 - 70);
-    ctx.lineTo(cw - 30, lh1 + 70);
-    ctx.lineTo(cw - 50, lh1 + 40);
-    ctx.lineTo(cw - 50, 40);
+    ctx.lineTo(cw - lw4, ch - lh2);
+    ctx.lineTo(cw - lw4, ch - lh1 - lh2);
+    ctx.lineTo(cw - lw5, ch - lh1 - lh2 - 30);
+    ctx.lineTo(cw - lw5, lh1 + lh2 + 30);
+    ctx.lineTo(cw - lw4, lh1 + lh2);
+    ctx.lineTo(cw - lw4, lh2);
     
-    ctx.lineTo(cw - lw1 - 70, 40);
-    ctx.lineTo(cw - lw1 - 100, 20);
-    ctx.lineTo(cw - lw1 - 150, 20);
-    ctx.lineTo(lw1 + 100, 20);
-    ctx.lineTo(lw1 + 70, 40);
-    ctx.lineTo(50, 40);
+    ctx.lineTo(cw - (lw1 + lw4), lh2);
+    ctx.lineTo(cw - (lw1 + lw4 + 15), lh3);
+    ctx.lineTo(lw1 + lw4 + 15, lh3);
+    ctx.lineTo(lw1 + lw4, lh2);
+    ctx.lineTo(lw4, lh2);
 
     ctx.stroke();
 
