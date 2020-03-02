808e959b61f99de9ca7d82fd1514bfd1f9552edd
Oleh WebDev <oleh.webdev@gmail.com>
2020-03-02 22:42:19 +0200

    0.1.10

--- js/canvas.js
+++ js/canvas.js
@@ -36,8 +36,8 @@
 
     var lw1 = m(wp(20), 200);
     var lw2 = m(wp(4), 40);
-    var lw4 = 40;
-    var lw5 = lw4 - 20;
+    var lw4 = 30;
+    var lw5 = lw4 - 15;
     var lw3 = cw - (lw1 + lw4 + 60 - lh3 + 15 + lh2 + 5 + lw2 + 30)*2;
 
     // BLACK BG
@@ -45,8 +45,8 @@
     ctx.beginPath();
     ctx.moveTo(lw4, lh2);
     ctx.lineTo(lw4, lh1 + lh2);
-    ctx.lineTo(lw5, lh1 + lh2 + 30);
-    ctx.lineTo(lw5, ch - lh1 - lh2 - 30);
+    ctx.lineTo(lw5, lh1 + lh2 + 20);
+    ctx.lineTo(lw5, ch - lh1 - lh2 - 20);
     ctx.lineTo(lw4, ch - lh1 - lh2);
     ctx.lineTo(lw4, ch - lh2);
     ctx.lineTo(0, ch);
@@ -92,8 +92,8 @@
     ctx.moveTo(cw, ch);
     ctx.lineTo(cw - lw4, ch - lh2);
     ctx.lineTo(cw - lw4, ch - lh1 - lh2);
-    ctx.lineTo(cw - lw5, ch - lh1 - lh2 - 30);
-    ctx.lineTo(cw - lw5, lh1 + lh2 + 30);
+    ctx.lineTo(cw - lw5, ch - lh1 - lh2 - 20);
+    ctx.lineTo(cw - lw5, lh1 + lh2 + 20);
     ctx.lineTo(cw - lw4, lh1 + lh2);
     ctx.lineTo(cw - lw4, lh2);
     ctx.lineTo(cw, 0);
@@ -124,8 +124,8 @@
     ctx.moveTo(lw4, lh2);
     
     ctx.lineTo(lw4, lh1 + lh2);
-    ctx.lineTo(lw5, lh1 + lh2 + 30);
-    ctx.lineTo(lw5, ch - lh1 - lh2 - 30);
+    ctx.lineTo(lw5, lh1 + lh2 + 20);
+    ctx.lineTo(lw5, ch - lh1 - lh2 - 20);
     ctx.lineTo(lw4, ch - lh1 - lh2);
     ctx.lineTo(lw4, ch - lh2);
 
@@ -153,8 +153,8 @@
 
     ctx.lineTo(cw - lw4, ch - lh2);
     ctx.lineTo(cw - lw4, ch - lh1 - lh2);
-    ctx.lineTo(cw - lw5, ch - lh1 - lh2 - 30);
-    ctx.lineTo(cw - lw5, lh1 + lh2 + 30);
+    ctx.lineTo(cw - lw5, ch - lh1 - lh2 - 20);
+    ctx.lineTo(cw - lw5, lh1 + lh2 + 20);
     ctx.lineTo(cw - lw4, lh1 + lh2);
     ctx.lineTo(cw - lw4, lh2);
     
@@ -166,6 +166,100 @@
 
     ctx.stroke();
 
-    // ctx.fill();
+
+    // ORANGE!!!
+
+    ctx.beginPath();
+    ctx.lineWidth = 1.5;
+    ctx.strokeStyle = '#ffa200';
+    ctx.lineCap = 'square';
+    ctx.lineJoin = 'round';
+    ctx.shadowBlur = 10;
+    ctx.shadowColor = '#d47e0f';
+    
+    // TOP LEFT
+    ctx.arc(lw4/2 + lw1*0.9, lh2*0.4, 3.5, 0, Math.PI*2);
+    ctx.moveTo(lw4/2 + lw1*0.9 - 4, lh2*0.4);
+    ctx.lineTo(lw4/2 + lw1*0.4, lh2*0.4);
+    ctx.lineTo(lw4/2 + lw1*0.4 - 5, lh2*0.4 + 5);
+    ctx.lineTo(lw4/3, lh2*0.4 + 5);
+    ctx.lineTo(lw4/3, lh2*2);
+    ctx.lineTo(lw4/3 + 8, lh2*2 + 8);
+    ctx.lineTo(lw4/3 + 8, lh1*0.9);
+    ctx.arc(lw4/3 + 8, lh1*0.9 + 5, 3.5, 0, Math.PI*2);
+
+    ctx.stroke();
+
+    // TOP RIGHT
+    ctx.beginPath();
+    ctx.arc(cw - (lw4/2 + lw1*0.9), lh2*0.4, 3.5, 0, Math.PI*2);
+    ctx.moveTo(cw - (lw4/2 + lw1*0.9 - 4), lh2*0.4);
+    ctx.lineTo(cw - (lw4/2 + lw1*0.4), lh2*0.4);
+    ctx.lineTo(cw - (lw4/2 + lw1*0.4 - 5), lh2*0.4 + 5);
+    ctx.lineTo(cw - lw4/3, lh2*0.4 + 5);
+    ctx.lineTo(cw - lw4/3, lh2*2);
+    ctx.lineTo(cw - (lw4/3 + 8), lh2*2 + 8);
+    ctx.lineTo(cw - (lw4/3 + 8), lh1*0.9);
+    ctx.arc(cw - (lw4/3 + 8), lh1*0.9 + 5, 3.5, 0, Math.PI*2);
+    ctx.stroke();
+
+    // LEFT
+    ctx.beginPath();
+    ctx.arc(lw4/2 - 4, ch - (lh1*0.9 + lh2), 3.5, 0, Math.PI*2);
+    ctx.moveTo(lw4/2 - 4, ch - (lh1*0.9 + lh2) + 5);
+    ctx.lineTo(lw4/2 - 4, ch - (lh1*0.7 + lh2));
+    ctx.lineTo(lw4/2 + 2, ch - (lh1*0.7 + lh2) + 6);
+    ctx.lineTo(lw4/2 + 2, ch - (lh2*1.5));
+    ctx.arc(lw4/2 + 2, ch - (lh2*1.5) + 5, 3.5, 0, Math.PI*2);
+    ctx.stroke();
+
+    // RIGHT
+    ctx.beginPath();
+    ctx.arc(cw - (lw4/2 - 4), ch - (lh1*0.9 + lh2), 3.5, 0, Math.PI*2);
+    ctx.moveTo(cw - (lw4/2 - 4), ch - (lh1*0.9 + lh2) + 5);
+    ctx.lineTo(cw - (lw4/2 - 4), ch - (lh1*0.7 + lh2));
+    ctx.lineTo(cw - (lw4/2 + 2), ch - (lh1*0.7 + lh2) + 6);
+    ctx.lineTo(cw - (lw4/2 + 2), ch - (lh2*1.5));
+    ctx.arc(cw - (lw4/2 + 2), ch - (lh2*1.5) + 5, 3.5, 0, Math.PI*2);
+    ctx.stroke();
+
+    // BOTTOM LEFT 1
+    ctx.beginPath();
+    ctx.arc(lw4*1.5, ch - lh2*0.6, 3.5, 0, Math.PI*2);
+    ctx.moveTo(lw4*1.5 + 5, ch - lh2*0.6);
+    ctx.lineTo(lw4*2.5, ch - lh2*0.6);
+    ctx.lineTo(lw4*2.5 + 7, ch - lh2*0.6 + 7);
+    ctx.lineTo(lw4*4, ch - lh2*0.6 + 7);
+    ctx.moveTo(lw4*4 + 8, ch - lh2*0.6 + 7);
+    ctx.arc(lw4*4 + 5, ch - lh2*0.6 + 7, 3.5, 0, -Math.PI*2);
+    ctx.stroke();
+    
+    // BOTTOM RIGHT 1
+    ctx.beginPath();
+    ctx.arc(cw - (lw4*1.5), ch - lh2*0.6, 3.5, 0, Math.PI*2);
+    ctx.moveTo(cw - (lw4*1.5 + 5), ch - lh2*0.6);
+    ctx.lineTo(cw - (lw4*2.5), ch - lh2*0.6);
+    ctx.lineTo(cw - (lw4*2.5 + 7), ch - lh2*0.6 + 7);
+    ctx.lineTo(cw - (lw4*4), ch - lh2*0.6 + 7);
+    ctx.arc(cw - (lw4*4 + 5), ch - lh2*0.6 + 7, 3.5, 0, Math.PI*2);
+    ctx.stroke();
+
+    // BOTTOM LEFT 2
+    ctx.beginPath();
+    ctx.arc(lw4*5.5, ch - lh2*0.5, 3.5, 0, Math.PI*2);
+    ctx.moveTo(lw4*5.5 + 5, ch - lh2*0.5);
+    ctx.lineTo(lw4*7.5, ch - lh2*0.5);
+    ctx.moveTo(lw4*7.5 + 8, ch - lh2*0.5);
+    ctx.arc(lw4*7.5 + 5, ch - lh2*0.5, 3.5, 0, -Math.PI*2);
+    ctx.stroke();
+    
+    // BOTTOM RIGHT 2
+    ctx.beginPath();
+    ctx.arc(cw - (lw4*5.5), ch - lh2*0.5, 3.5, 0, Math.PI*2);
+    ctx.moveTo(cw - (lw4*5.5 + 5), ch - lh2*0.5);
+    ctx.lineTo(cw - (lw1*0.9), ch - lh2*0.5);
+    ctx.arc(cw - (lw1*0.9), ch - lh2*0.5, 3.5, 0, -Math.PI*2);
+    ctx.stroke();
+
   }
 })();
